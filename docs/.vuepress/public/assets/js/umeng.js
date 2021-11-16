e9aae4f2237a94393b9de551d2343c05d3423f31
Marvin Zhang <tikazyq@163.com>
2021-11-16 20:59:15 +0800

    updated umeng.js

--- docs/.vuepress/public/assets/js/umeng.js
+++ docs/.vuepress/public/assets/js/umeng.js
@@ -5,4 +5,20 @@
   j.id = 'beacon-aplus'
   j.src = 'https://d.alicdn.com/alilog/mlog/aplus/' + i + '.js'
   f.parentNode.insertBefore(j, f)
-})(window, document, 'script', 'aplus_queue', '203467608')
+})(window, document, 'script', 'aplus_queue', '203467608');
+
+(async function () {
+    //集成应用的appKey
+    window.aplus_queue.push({
+      action: 'aplus.setMetaInfo',
+      arguments: ['appKey', '6193a6b7e014255fcb7d60b8']
+    })
+
+    //发送pv
+    const { aplus_queue } = window
+    window.aplus_queue.push({
+      action: 'aplus.sendPV',
+      arguments: [{ is_auto: true }, {}]
+    })
+
+})()
