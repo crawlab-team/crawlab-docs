1550a4b44d1f7fd0cb6cf60a0b1e69b49332ec41
Marvin Zhang <tikazyq@163.com>
2022-10-15 14:34:58 +0800

    feat(tracking): added tracking code to help improve user experience
    google analytics and microsoft clarity

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,9 +16,19 @@ module.exports = {
     },
   },
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['meta', { name: 'default-lang', content: process.env.CRAWLAB_LANG || 'zh' }],
-    ['script', { src: '/assets/js/umeng.js' }]
+    ['link', {rel: 'icon', href: '/favicon.ico'}],
+    ['meta', {name: 'default-lang', content: process.env.CRAWLAB_LANG || 'zh'}],
+    ['script', {src: '/assets/js/umeng.js'}],
+    ['script', {async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-G9445D9CW9'}],
+    ['script', {}, `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-G9445D9CW9');`],
+    ['script', {}, `(function(c,l,a,r,i,t,y){
+c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "e3a7lpontr");`]
   ],
   plugins: [
     '@vuepress/active-header-links',
